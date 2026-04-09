33a713a999ffcc46408889904f957933e364f189
Steve R <bravo197336@sky.com>
2026-04-09 17:35:29 +0100

    Update mission error handling and save point

--- src/missions/errors.ts
+++ src/missions/errors.ts
@@ -5,4 +5,4 @@ export class InvalidMissionTransitionError extends Error {
     super(message);
     this.name = "InvalidMissionTransitionError";
   }
-} i am not changing addSyntheticLeadingComment
+} 
