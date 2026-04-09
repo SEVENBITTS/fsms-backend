fadeb5598a95db5a46eb7f886580c7dea48aa736
Steve R <bravo197336@sky.com>
2026-04-09 16:56:04 +0100

    Test: change core logic without save point

--- src/missions/errors.ts
+++ src/missions/errors.ts
@@ -1,6 +1,8 @@
+import { addSyntheticLeadingComment } from "typescript";
+
 export class InvalidMissionTransitionError extends Error {
   constructor(message: string) {
     super(message);
     this.name = "InvalidMissionTransitionError";
   }
-}
+} i am not changing addSyntheticLeadingComment
